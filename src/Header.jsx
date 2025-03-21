03d75fa519fe2e62248eeca46a38a4c1f1df20d1
272853 <163885786+272853@users.noreply.github.com>
2025-03-21 16:58:21 +0100

    setup fix

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+
+function Header(){
+    return(
+        <div>
+            <button>Home</button>
+            <button>ToolBar</button>
+            <input type="search" placeholder="Serch:"/>
+            <button>Account</button>
+
+        </div>
+    );
+}
+
+export default Header;
