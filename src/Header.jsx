363269d49c58d424f7e3d1aa7eb1afd7618609ba
272853 <163885786+272853@users.noreply.github.com>
2025-03-24 20:43:16 +0100

    Header+ToolBar

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,10 +2,10 @@
 function Header(){
     return(
         <div className='Header'>
-            <button name="home">Home</button>
-            <button name="toolBar">ToolBar</button>
-            <input type="search" placeholder="Serch:"/>
-            <button name="account" id="acc">Account</button>
+            <button className="hbutton" name="home">Home</button>
+            <button className="hbutton" name="toolBar">ToolBar</button>
+            <input type="search" id="search" placeholder="Serch:"/>
+            <button name="account" id="acc">A</button>
 
         </div>
     );
