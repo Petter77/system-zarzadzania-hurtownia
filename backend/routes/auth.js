be1d967e03428c6037b3eadcf47a0574c54cecd5
maciej790 <maciejmw126@gmail.com>
2025-04-18 19:51:37 +0200

    fixed bugs with auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,27 +6,27 @@ const checkIfUserName = require('../middlewares/auth/checkIfUserName');
 
 const SECRET_KEY = 'secret';
 
-// login user with checkIfUserName middleware for check if username is busy or username exists in db
+// login user with checkIfusername middleware for check if username is busy or username exists in db
 router.post('/login', checkIfUserName(false), async (req, res) =>{
-    const hashedPassword = req.user.password;
-    const {UserPassword} = req.body;
+    const hashedPassword = req.user.password_hash;
+    const {password} = req.body;
     
-    if(!UserPassword) return res.status(400).json('Wypełnij wszystkie pola!');
-    const isPasswordMatch = await bcrypt.compare(UserPassword, hashedPassword);
+    if(!password) return res.status(400).json('Wypełnij wszystkie pola!');
+    const isPasswordMatch = await bcrypt.compare(password, hashedPassword);
     if (!isPasswordMatch) return res.status(401).json('Błędny login lub hasło!');
-    const UserID = req.user.id;
-    const UserName = req.user.login
-    const rola = req.user.rola;
-    // zrobic obiekt w ktorym bedzie token, userid i username
-    // ten obiekt zapisac w sesji na frontendzie, wtedy frontend bedzie mial username bez koniecznosci zapytania
-    // na backendzie uwzglednic ze w tokenie jest obiekt a sam token to pole tego obiektu
-    const token = jwt.sign({UserID}, SECRET_KEY, {expiresIn: "1h"});
+
+    const userId = req.user.id;
+    const username = req.user.username
+    const role = req.user.role;
+
+    const token = jwt.sign({userId}, SECRET_KEY, {expiresIn: "1h"});
     const user = {
       token,
-      UserID,
-      UserName,
-      rola
+      userId,
+      username,
+      role
     }
+
     res.status(200).json(user);
 })
 
